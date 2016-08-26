2661e9a574898e54f8fc2e025a4bf5b984168cc0
Grayson A.C. Laramore <info@sillycoyote.com>
2016-08-25 20:50:14 -0700

    fixed shortcode texture, moved filters, added chunk title

--- index.php
+++ index.php
@@ -12,15 +12,6 @@ defined('ABSPATH') or die("Ya, took a wrong turn at Albuquerque, mac!"); // Don'
 	License: GPL2
 */
 
-
-
-
-
-// This moves the_content filter 'wptexturize' to after 'do_shortcode'. This solves a curly quote issue in nested plugins.
-	   // This may be gone if future additions, I just need to figure out regex non-sense
-	remove_filter('the_content', 'wptexturize');
-	add_filter('the_content', 'wptexturize', 12, 1);
-
 include 'lib/shortcode_skivdiv.php';	// SkivDivs-chunks
 include 'lib/shortcode_bloginfo.php';	// [bloginfo]
 include 'lib/shortcode_bucket.php';		// [bucket]
@@ -30,7 +21,7 @@ include 'lib/shortcode_iframe.php';		// [iframe]
 include 'lib/shortcode_lorem.php'; 		// [lorem]
 include 'lib/shortcode_newsfeed.php';	// [newsfeed]
 
-include 'lib/shortcode_chunk.php';	// SkivDivs-chunks
-
+include 'lib/shortcode_chunk.php';
+include 'lib/filters-chunk.php';
 
 ?>
--- lib/shortcode_chunk.php
+++ lib/shortcode_chunk.php
@@ -1,86 +1,26 @@
 <?php
 
-# 24AUG16
-
-$chunk_tags = array( 'chunk' );
-$chunk_tags = apply_filters( 'chunk_tags', $chunk_tags );
+$chunk_tags = apply_filters( 'chunk_tags', $array( 'chunk' ) );
 foreach( $chunk_tags as $tag ) {
 	add_shortcode( $tag, 'shortcode_chunk' );
 }
 
-
+// Chunk Shortcode function
 function shortcode_chunk ( $atts, $content = null, $tag ) {
 
 	// Shortcode Attributes
 		$attr = shortcode_atts( array(
 			'id'            => NULL,
-			'class'         => '',
+			'class'         => NULL,
 			'style'         => NULL,
 			'title'         => NULL,
 			'key'           => NULL,
 			'autop'         => TRUE,
 		), $atts, $tag );
 
-
-	// Content
-		
-		$content = apply_filters( 'chunk_content', $content );
-	// Container ID
-		if ( is_null($attr['id']) ) {
-			$containerID = '';
-		} else {
-			$containerID = ' id="' . $attr['id'] . '"';
-		}
-
-	// Container classes
-		$classes = ' class="';
-			$classes .= 'chunk-' . $tag;
-			if ( $attr['class'] ) {
-				$classes .= ' ' . $attr['class'];
-			}
-		$classes .= '"';
-
-	// Container Styles
-		if ( is_null($attr['class']) ) {
-			$containerStyles = '';
-		} else {
-			$containerStyles = ' style="' . $attr['style'] . '"';
-		}
-
-	// Return output
-		$output = '<div' . $containerID . $classes . $containerStyles .'>';
-			$output .= $content;	
-		$output .= '</div>';
+		$content = apply_filters( 'chunk_content', $content, $tag, $attr );
+		$output  = apply_filters( 'chunk_wrapper', $content, $tag, $attr );
 		return $output; 
 
 }
-
-
-// Chunk Content filters
-function filter_chunk_content_autop ($content) {
-	if ( $attr['autop'] === TRUE ) {
-		$content = wpautop($content);
-	}
-	return $content;
-}
-
-function filter_chunk_content_nesting ($content) {
-	if ( strpos( $content, '[=' ) !== FALSE ) {
-		$content = str_replace ( '[=', '[', $content);
-
-		#$content = preg_replace('/{([^}]*)}/', $str, $matches);
-		#$content = str_replace('&#8220;', '&quot;', $content);
-		#$content = str_replace('&#8221;', '&quot;', $content);
-	}
-     return $content;
-}
-
-function filter_chunk_content_doshortcode($content) {
-	// renders internal shortcodes 
-	return do_shortcode($content, TRUE);
-}
-
-add_filter( 'chunk_content', 'filter_chunk_content_nesting', 100, 1 );
-add_filter( 'chunk_content', 'filter_chunk_content_autop', 150, 1 );
-add_filter( 'chunk_content', 'filter_chunk_content_doshortcode', 200, 1 );
 

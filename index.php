d3c0e12b0769a82f6cd366503cbbf7a66e5005af
Grayson A.C. Laramore <info@sillycoyote.com>
2016-08-25 18:52:02 -0700

    updated to Web Chunk information, added chunk include

--- index.php
+++ index.php
@@ -2,16 +2,18 @@
 defined('ABSPATH') or die("Ya, took a wrong turn at Albuquerque, mac!"); // Don't worry about this portion, it's for security folks.
 
 /*
-	Plugin Name: skivdiv-chunks
+	Plugin Name: Web Chunks
 	Plugin URI: https://github.com/atelierabbey/Skivdiv-chunks
 	GitHub Plugin URI: https://github.com/atelierabbey/Skivdiv-chunks
 	GitHub Branch: master
-	Description: The ideal primer for a custom post type plugin. Simple find and replace 'skivvy_post_type', the singular name, and plural name
-	Version: 11Nov15
+	Description: Chunk up your website to create flexible, modular, and friendly divisions all across your WordPress site.
+	Version: 23Aug16
 	Author: Grayson A.C. Laramore
 	License: GPL2
 */
 
+
+include 'lib/shortcode_skivdiv.php';	// SkivDivs-chunks
 
 
 include 'lib/shortcode_bloginfo.php';	// [bloginfo]
@@ -21,8 +23,8 @@ include 'lib/shortcode_icon.php';		// [icon key="mapping"]
 include 'lib/shortcode_iframe.php';		// [iframe]
 include 'lib/shortcode_lorem.php'; 		// [lorem]
 include 'lib/shortcode_newsfeed.php';	// [newsfeed]
-include 'lib/shortcode_skivdiv.php';	// SkivDivs-chunks
 
+include 'lib/shortcode_chunk.php';	// SkivDivs-chunks
 
 
 ?>
